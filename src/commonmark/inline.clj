cac936c8d9d20ee6c7e6073d83cc7d60826d8538
eureton <agis@mailbox.org>
2021-05-31 17:43:43 +0300

    emphasis: delimeter runs, * support

--- src/commonmark/inline.clj
+++ src/commonmark/inline.clj
@@ -1,4 +1,5 @@
-(ns commonmark.inline)
+(ns commonmark.inline
+  (:require [clojure.string :as string]))
 
 (comment "Code spans
 
@@ -33,3 +34,191 @@ OK      a single space character is removed from the front and back.")
      :content (-> (or spaced-content non-spaced-content)
                   (clojure.string/replace #"(?:\r|\n|\r\n)"  " "))}))
 
+(comment "Emphasis, Strong Emphasis
+
+OK  A delimiter run is
+OK  either a sequence of one or more * characters
+OK  that is not preceded or followed by a non-backslash-escaped * character,
+OK  or a sequence of one or more _ characters that is not preceded or followed by a non-backslash-escaped _ character.
+
+OK  A left-flanking delimiter run is a delimiter run that is
+OK  (1) not followed by Unicode whitespace,
+OK  and either (2a) not followed by a punctuation character,
+OK  or (2b) followed by a punctuation character
+OK  and preceded by Unicode whitespace or a punctuation character.
+
+OK  A right-flanking delimiter run is a delimiter run that is
+OK  (1) not preceded by Unicode whitespace,
+OK  and either (2a) not preceded by a punctuation character,
+OK  or (2b) preceded by a punctuation character
+OK  and followed by Unicode whitespace
+OK  or a punctuation character.
+
+OK  The beginning and the end of the line count as Unicode whitespace.
+
+    The following rules define emphasis and strong emphasis:
+      1. A single * character can open emphasis iff it is part of a left-flanking delimiter run.
+      2. A single _ character can open emphasis iff it is part of a left-flanking delimiter run and
+         either (a) not part of a right-flanking delimiter run
+         or (b) part of a right-flanking delimiter run preceded by punctuation.
+      3. A single * character can close emphasis iff it is part of a right-flanking delimiter run.
+      4. A single _ character can close emphasis iff it is part of a right-flanking delimiter run and
+         either (a) not part of a left-flanking delimiter run
+         or (b) part of a left-flanking delimiter run followed by punctuation.
+      5. A double ** can open strong emphasis iff it is part of a left-flanking delimiter run.
+      6. A double __ can open strong emphasis iff it is part of a left-flanking delimiter run and
+         either (a) not part of a right-flanking delimiter run
+         or (b) part of a right-flanking delimiter run preceded by punctuation.
+      7. A double ** can close strong emphasis iff it is part of a right-flanking delimiter run.
+      8. A double __ can close strong emphasis iff it is part of a right-flanking delimiter run and
+         either (a) not part of a left-flanking delimiter run
+         or (b) part of a left-flanking delimiter run followed by punctuation.
+      9. Emphasis begins with a delimiter that can open emphasis
+         and ends with a delimiter that can close emphasis,
+         and that uses the same character (_ or *) as the opening delimiter.
+         The opening and closing delimiters must belong to separate delimiter runs.
+         If one of the delimiters can both open and close emphasis,
+         then the sum of the lengths of the delimiter runs containing the opening and closing delimiters
+         must not be a multiple of 3 unless both lengths are multiples of 3.
+     10. Strong emphasis begins with a delimiter that can open strong emphasis
+         and ends with a delimiter that can close strong emphasis,
+         and that uses the same character (_ or *) as the opening delimiter.
+         The opening and closing delimiters must belong to separate delimiter runs.
+         If one of the delimiters can both open and close strong emphasis,
+         then the sum of the lengths of the delimiter runs containing the opening and closing delimiters
+         must not be a multiple of 3 unless both lengths are multiples of 3.
+     11. A literal * character cannot occur at the beginning
+         or end of *-delimited emphasis
+         or **-delimited strong emphasis,
+         unless it is backslash-escaped.
+     12. A literal _ character cannot occur at the beginning
+         or end of _-delimited emphasis
+         or __-delimited strong emphasis,
+         unless it is backslash-escaped.
+
+    Where rules 1–12 above are compatible with multiple parsings, the following principles resolve ambiguity:
+     13. The number of nestings should be minimized.
+         Thus an interpretation <strong>...</strong> is always preferred to <em><em>...</em></em>.
+     14. An interpretation <em><strong>...</strong></em> is always preferred to <strong><em>...</em></strong>.
+     15. When two potential emphasis or strong emphasis spans overlap,
+         so that the second begins before the first ends
+         and ends after the first ends,
+         the first takes precedence.
+         Thus *foo _bar* baz_ is parsed as <em>foo _bar</em> baz_
+         rather than *foo <em>bar* baz</em>.
+     16. When there are two potential emphasis or strong emphasis spans
+         with the same closing delimiter,
+         the shorter one (the one that opens later) takes precedence.
+         Thus **foo **bar baz** is parsed as **foo <strong>bar baz</strong>
+         rather than <strong>foo **bar baz</strong>.
+     17. Inline code spans, links, images, and HTML tags group more tightly than emphasis.
+         So, when there is a choice between an interpretation that contains one of these elements
+         and one that does not, the former always wins.
+         Thus *[foo*] (bar) is parsed as *<a href= "bar">foo*</a>
+         rather than as <em> [foo</em>] (bar).")
+
+(comment
+  (def emphasis-delimeter-re
+    (let [underscore #"(?<!_)_+(?!_)"
+          asterisk #"(?<!\*)\*+(?!\*)"]
+      (re-pattern (str "(?:" asterisk "|" underscore ")"))))
+
+  (def left-flanking-emphasis-delimeter-run-re
+    (let [no-whitespace (re-pattern (str emphasis-delimeter-re #"(?!\p{IsWhite_Space}|$)"))
+          no-punctuation (re-pattern (str no-whitespace #"(?!\p{IsPunctuation})"))
+          punctuation (re-pattern
+                        (str #"(?<=^|\p{IsWhite_Space}|\p{IsPunctuation})"
+                             no-whitespace
+                             #"(?=\p{IsPunctuation})"))]
+      (re-pattern (str "(?:" no-punctuation "|" punctuation ")"))))
+
+  (def right-flanking-emphasis-delimeter-run-re
+    (let [no-whitespace (re-pattern (str #"(?<!^|\p{IsWhite_Space})" emphasis-delimeter-re))
+          no-punctuation (re-pattern (str #"(?<!\p{IsPunctuation})" no-whitespace))
+          punctuation (re-pattern
+                        (str #"(?<=\p{IsPunctuation})"
+                             no-whitespace
+                             #"(?=\p{IsWhite_Space}|\p{IsPunctuation}|$)"))]
+      (re-pattern (str "(?:" no-punctuation "|" punctuation ")"))))
+
+  (def emphasis-re
+    (re-pattern (str left-flanking-emphasis-delimeter-run-re
+                     #"(\p{Print}*)"
+                     right-flanking-emphasis-delimeter-run-re))))
+
+(defn emphasis-delimeter-re
+  [character length]
+  (let [escaped (string/escape "*" {\* "\\*"})]
+    (re-pattern (str "(?<!" escaped ")"
+                     escaped "{" length "}"
+                     "(?!" escaped ")"))))
+
+(defn left-flanking-emphasis-delimeter-run-re
+  [character length]
+  (let [delimeter (emphasis-delimeter-re character length)
+        no-whitespace (re-pattern (str delimeter #"(?!\p{IsWhite_Space}|$)"))
+        no-punctuation (re-pattern (str no-whitespace #"(?!\p{IsPunctuation})"))
+        punctuation (re-pattern
+                      (str #"(?<=^|\p{IsWhite_Space}|\p{IsPunctuation})"
+                           no-whitespace
+                           #"(?=\p{IsPunctuation})"))]
+    (re-pattern (str "(?:" no-punctuation "|" punctuation ")"))))
+
+(defn right-flanking-emphasis-delimeter-run-re
+  [character length]
+  (let [delimeter (emphasis-delimeter-re character length)
+        no-whitespace (re-pattern (str #"(?<!^|\p{IsWhite_Space})" delimeter))
+        no-punctuation (re-pattern (str #"(?<!\p{IsPunctuation})" no-whitespace))
+        punctuation (re-pattern
+                      (str #"(?<=\p{IsPunctuation})"
+                           no-whitespace
+                           #"(?=\p{IsWhite_Space}|\p{IsPunctuation}|$)"))]
+    (re-pattern (str "(?:" no-punctuation "|" punctuation ")"))))
+
+(def emphasis-re
+  (re-pattern (str (left-flanking-emphasis-delimeter-run-re \* 1)
+                   #"(\p{Print}*)"
+                   (right-flanking-emphasis-delimeter-run-re \* 1))))
+
+(def strong-emphasis-re
+  (re-pattern (str (left-flanking-emphasis-delimeter-run-re \* 2)
+                   #"(\p{Print}*)"
+                   (right-flanking-emphasis-delimeter-run-re \* 2))))
+
+(defn emphasis
+  [string]
+  (when-some [[_ content] (re-find emphasis-re string)]
+    {:content content}))
+
+(defn strong-emphasis
+  [string]
+  (when-some [[_ content] (re-find strong-emphasis-re string)]
+    {:content content}))
+
+(comment
+  (emphasis "foo*bar*")
+  (emphasis "foo*bar*" "*abc*")
+  (strong-emphasis "**abc**")
+  (re-find emphasis-re "_abc_")
+  (let [; left yes, right no
+        s01 "***abc"
+        s02 "*abc"
+        s03 "**\"abc \""
+        s04 "*\"abc \""
+        s05 "_\"abc \""
+        s06 "_abc"
+        ; left no, right yes
+        s07 "abc***"
+        s08 "abc_"
+        s09 "\"abc \"**"
+        s10 "\"abc \"_"
+        ; left yes, right yes
+        s11 "abc***def"
+        s12 "\"abc \"_\"def \""
+        ; left no, right no
+        s13 "abc *** def"
+        s14 "a _ b"]
+    (map #(vector (re-find left-flanking-emphasis-delimeter-run-re %)
+                  (re-find right-flanking-emphasis-delimeter-run-re %)) [s01 s02 s03 s04 s05 s06
+                                                                         s07 s08 s09 s10 s11 s12
+                                                                         s13 s14])))
--- test/commonmark/inline_test.clj
+++ test/commonmark/inline_test.clj
@@ -73,3 +73,74 @@
          "`foo"
          "`foo``bar``")))
 
+(deftest emphasis-test
+  (testing "opening with *"
+    (testing "minimal"
+      (is (= (-> "*foo bar*" emphasis :content)
+             "foo bar")))
+
+    (testing "opening * followed by whitespace => not emphasis"
+      (is (nil? (emphasis "a * foo bar*"))))
+
+    (testing "opening * preceded by alphanumeric and followed by punctuation => not emphasis"
+      (is (nil? (emphasis "a*\"foo\"*"))))
+
+    (testing "unicode nonbreaking spaces => not emphasis"
+      (are [c] (nil? (emphasis (str "*" c "a *")))
+           \u0009
+           \u000A
+           \u000C
+           \u000D
+           \u00A0
+           \u1680
+           \u2000
+           \u2001
+           \u2002
+           \u2003
+           \u2004
+           \u2005
+           \u2006
+           \u2007
+           \u2008
+           \u2009
+           \u200A
+           \u202F
+           \u205F
+           \u3000))
+
+    (testing "intraword"
+      (are [s c] (= (-> s emphasis :content)
+                    c)
+           "foo*bar*" "bar"
+           "5*6*78"   "6")))
+
+  (testing "opening with _"
+    (testing "minimal"
+      (is (= (-> "_foo bar_" emphasis :content)
+             "foo bar")))
+
+    (testing "opening _ followed by whitespace => not emphasis"
+      (is (nil? (emphasis "_ foo bar_"))))
+
+    (testing "opening _ preceded by alphanumeric and followed by punctuation => not emphasis"
+      (is (nil? (emphasis "a_\"foo \"_"))))
+
+    (testing "intraword"
+      (are [s] (nil? (emphasis s))
+           "foo_bar_"
+           "5_6_78"))))
+
+; TODO
+(comment "
+   Here _ does not generate emphasis, because the first delimiter run is right-flanking and the second
+   left-flanking:
+
+aa_ "bb"_cc
+<p>aa_&quot;bb&quot;_cc</p>
+
+   This is emphasis, even though the opening delimiter is both left- and right-flanking, because it is preceded by
+   punctuation:
+
+foo-_ (bar)_
+<p>foo-<em> (bar)</em></p>
+")
