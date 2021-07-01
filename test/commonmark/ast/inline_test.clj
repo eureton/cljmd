0dccc7183aedeef2c8f3ec040521e33d772f00ae
eureton <agis@mailbox.org>
2021-07-01 12:09:37 +0300

    cleanup

--- test/commonmark/ast/inline_test.clj
+++ test/commonmark/ast/inline_test.clj
@@ -1,37 +1,33 @@
 (ns commonmark.ast.inline-test
   (:require [clojure.test :refer :all]
-            [commonmark.ast.inline :refer :all]))
+            [commonmark.ast.inline :refer :all]
+            [commonmark.ast.common :refer [node]]))
 
 (deftest from-string-test
   (testing "pun nil"
     (is (nil? (from-string nil))))
 
   (testing "minimal"
-    (let [txt-node {:data {:tag :txt
-                           :content "xyz"}}]
+    (let [txt-node (node {:tag :txt :content "xyz"})]
       (testing "text"
         (is (= (-> "xyz" from-string (get-in [:children 0]))
                txt-node)))
 
       (testing "code span"
         (is (= (-> "`xyz`" from-string (get-in [:children 0]))
-               {:data {:tag :cs}
-                :children [txt-node]})))
+               (node {:tag :cs} [txt-node]))))
 
       (testing "emphasis"
         (are [s] (= (-> s from-string (get-in [:children 0]))
-                    {:data {:tag :em}
-                     :children [txt-node]})
+                    (node {:tag :em} [txt-node]))
              "*xyz*"
              "_xyz_"))))
 
   (testing "nested emphasis"
-    (let [em-tree {:data {:tag :em}
-                   :children [{:data {:tag :txt :content "("}}
-                              {:data {:tag :em}
-                               :children [{:data {:tag :txt
-                                                  :content "xyz"}}]}
-                              {:data {:tag :txt :content ")"}}]}]
+    (let [em-tree (node {:tag :em}
+                        [(node {:tag :txt :content "("})
+                         (node {:tag :em} [(node {:tag :txt :content "xyz"})])
+                         (node {:tag :txt :content ")"})])]
       (are [s] (= (-> s from-string (get-in [:children 0]))
                   em-tree)
            "_(_xyz_)_"
