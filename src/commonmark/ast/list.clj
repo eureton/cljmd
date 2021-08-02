8470298491788b58547a947c5897ed606beb80cc
eureton <agis@mailbox.org>
2021-08-02 20:26:43 +0300

    list-related stuff in its own ns

---
+++ src/commonmark/ast/list.clj
@@ -0,0 +1,61 @@
+(ns commonmark.ast.list
+  (:require [flatland.useful.fn :as ufn]
+            [commonmark.ast.common :refer [node add]]))
+
+(defn marker-type
+  "Classifies as either :bullet or :ordered."
+  [marker]
+  (if (some? (re-find #"[-+*]" marker))
+    :bullet
+    :ordered))
+
+(defn delimiter
+  "Classifies as either \"paren\" or \"period\" if ordered, nil otherwise."
+  [marker]
+  (as-> marker v
+        (re-find #"^\d{1,9}([.)])$" v)
+        (second v)
+        (ufn/fix v #{")"} "paren"
+                   #{"."} "period"
+                   nil)))
+
+(defn start
+  "The number to start from if ordered (as a string), nil otherwise."
+  [marker]
+  (->> marker
+       (re-find #"^(\d{1,9})[.)]$")
+       second))
+
+(defn sibling-items?
+  "True if items x and y belong in the same list, false otherwise."
+  [x y]
+  (and (-> x :data :tag (= :li))
+       (-> y :data :tag (= :li))
+       (or (= (-> x :data :marker)
+              (-> y :data :marker))
+           (and (= (-> x :data :marker marker-type)
+                   :ordered)
+                (= (-> x :data :marker marker-type)
+                   (-> y :data :marker marker-type))
+                (= (-> x :data :marker last)
+                   (-> y :data :marker last))))))
+
+(defn empty-for
+  "List AST node with no children, configured for marker."
+  [marker]
+  (let [list-type (-> marker marker-type name)
+        delimiter (delimiter marker)
+        start (start marker)]
+    (node (cond-> {:tag :list
+                   :type list-type
+                   :tight "true"}
+            delimiter (assoc :delimiter delimiter)
+            start (assoc :start start)))))
+
+(defn from-items
+  "AST node which represents a list comprising items."
+  [items]
+  (->> items
+       (map #(assoc % :data {:tag :li}))
+       (reduce add (-> items first :data :marker empty-for))))
+
