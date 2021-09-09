6cfef087feb7596ea02637ccbf91acc75f7de2a9
eureton <agis@mailbox.org>
2021-09-09 11:26:47 +0300

    separate ns for HTML-specific functions

---
+++ src/commonmark/html.clj
@@ -0,0 +1,23 @@
+(ns commonmark.html
+  (:require [clojure.string :as string]
+            [clojure.java.io :as java.io]
+            [cheshire.core :as cheshire]
+            [flatland.useful.fn :as ufn]
+            [commonmark.re.common :as re.common]))
+
+(def entity-map
+  "Authoritative HTML entity map, as sourced from html.spec.whatwg.org"
+  (-> "entities.json" java.io/resource java.io/reader cheshire/parse-stream))
+
+(defn unescape-entities
+  "Replaces HTML entities with the corresponding character."
+  [s]
+  (let [from-decnum (comp str char (ufn/to-fix zero? 0xFFFD))
+        decim (re.common/unescaped #"&#(\d{1,7});")
+        hex (re.common/unescaped #"&#[xX](\p{XDigit}{1,6});")
+        alpha (re.common/unescaped #"&\p{Print}+?;")]
+    (-> s
+        (string/replace decim #(-> % second Integer/parseInt from-decnum))
+        (string/replace hex #(-> % second (Integer/parseInt 16) from-decnum))
+        (string/replace alpha #(get-in entity-map [% "characters"] %)))))
+
