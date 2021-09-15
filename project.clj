db6989623797b3a7bff2c941f0fa9f8586917025
eureton <agis@mailbox.org>
2021-09-15 12:10:38 +0300

    project: fetch squirrel from clojars

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.incubator "0.1.4"]
-                 [squirrel "0.1.0"]
+                 [org.clojars.eureton/squirrel "0.1.0"]
                  [org.flatland/useful "0.11.6"]
                  [cheshire "5.10.0"]]
   :repl-options {:init-ns cljmd.core})
